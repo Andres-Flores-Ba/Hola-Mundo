b24994de2d9041809644f29771aafdf95b367f16
Andres Flores Bañuelos <159966414+Andres-Flores-Ba@users.noreply.github.com>
2024-02-19 23:54:29 +0000

    Version 1

---
+++ include/Alimento.hpp
@@ -0,0 +1,14 @@
+#pragma once
+class Alimento
+{
+private:
+    int Energia;
+public:
+    Alimento(int Energia) {
+        this->Energia = Energia;
+    }
+    int ExtraerEnergia(){
+        return this->Energia;
+    }
+    ~Alimento() {}
+};
--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <Mascota.hpp>
+#include <Alimento.hpp>
 using namespace std;
+
 int main()
 {
-    cout<<"Hola_"<<"23110155"<<endl;
+    Mascota m1;
+    Mascota m2;
+
+    Alimento a1(5);
+    Alimento a2(2);
+
+    m1.Comer(a1);
+    m2.Comer(a2);
+
+    cout<<"Mascota 1 Energia:"<<m1.LeerEnergia()<<endl
+        <<"Mascota 2 Energia:"<<m2.LeerEnergia()<<endl;
     return 0;
 }
